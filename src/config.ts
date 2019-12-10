bd771e489811c1205c010cbbff0d9f5b415f6d2a
Yonatan <yonatald@gmail.com>
2019-12-10 23:00:52 +0200

    basic functionality
    Signed-off-by: Yonatan <yonatald@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,8 @@
-export const redisHost = process.env.REDIS_URL;
-export const ClientId = process.env.SPIKE_CLIENT_ID;
-export const ClientSecret = process.env.SPIKE_CLIENT_SECRET;
-export const spikeURL = process.env.SPIKE_TOKEN_URL;
-export const tokenGrantType = process.env.PHONEBOOK_GRANT_TYPE;
-export const tokenAudience = process.env.PHONEBOOK_AUDIENCE;
-export const tokenRedisKeyName = process.env.PHONEBOOK_REDIS_KEY;
-export const spikePublicKeyFullPath = './key.pem';
-export const useRedis = true;
+export const ClientId = process.env.SPIKE_CLIENT_ID || 'enter_client_id';
+export const ClientSecret = process.env.SPIKE_CLIENT_SECRET || 'enter_client_secret';
+export const redisHost = process.env.REDIS_URL || '127.0.0.1:6379';
+export const spikeURL = process.env.SPIKE_TOKEN_URL || 'https://localhost:1337/oauth2/token';
+export const spikePublicKeyFullPath = process.env.SPIKE_PUBLIC_KEY_FULL_PATH || '/usr/src/app/utils/publickey.pem';
+
+export const host = process.env.SS_HOST || '0.0.0.0';
+export const port = process.env.SS_PROT || '8080';
