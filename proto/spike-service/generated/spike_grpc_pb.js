2e9242b8e18a261edaf99c2338a4aef4c0209626
AllKinds <yonatald@gmail.com>
2020-07-07 13:02:30 +0300

    Feature/auth code (#20)
    * feat: add authentication code flow
    
    * docs: add docmentation for the grpc functions in the service
    
    Signed-off-by: YonatanTal <yonatald@gmail.com>

--- proto/spike-service/generated/spike_grpc_pb.js
+++ proto/spike-service/generated/spike_grpc_pb.js
@@ -26,6 +26,28 @@ function deserialize_spike_SpikeToken(buffer_arg) {
   return spike_pb.SpikeToken.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
+function serialize_spike_ValidateAuthCodeTokenRequest(arg) {
+  if (!(arg instanceof spike_pb.ValidateAuthCodeTokenRequest)) {
+    throw new Error('Expected argument of type spike.ValidateAuthCodeTokenRequest');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_spike_ValidateAuthCodeTokenRequest(buffer_arg) {
+  return spike_pb.ValidateAuthCodeTokenRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_spike_ValidateAuthCodeTokenResponse(arg) {
+  if (!(arg instanceof spike_pb.ValidateAuthCodeTokenResponse)) {
+    throw new Error('Expected argument of type spike.ValidateAuthCodeTokenResponse');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_spike_ValidateAuthCodeTokenResponse(buffer_arg) {
+  return spike_pb.ValidateAuthCodeTokenResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
 function serialize_spike_ValidateTokenRequest(arg) {
   if (!(arg instanceof spike_pb.ValidateTokenRequest)) {
     throw new Error('Expected argument of type spike.ValidateTokenRequest');
@@ -71,6 +93,17 @@ var SpikeService = exports.SpikeService = {
     requestDeserialize: deserialize_spike_ValidateTokenRequest,
     responseSerialize: serialize_spike_ValidateTokenResponse,
     responseDeserialize: deserialize_spike_ValidateTokenResponse,
+  },
+  validateAuthCodeToken: {
+    path: '/spike.Spike/ValidateAuthCodeToken',
+    requestStream: false,
+    responseStream: false,
+    requestType: spike_pb.ValidateAuthCodeTokenRequest,
+    responseType: spike_pb.ValidateAuthCodeTokenResponse,
+    requestSerialize: serialize_spike_ValidateAuthCodeTokenRequest,
+    requestDeserialize: deserialize_spike_ValidateAuthCodeTokenRequest,
+    responseSerialize: serialize_spike_ValidateAuthCodeTokenResponse,
+    responseDeserialize: deserialize_spike_ValidateAuthCodeTokenResponse,
   },
 };
 
